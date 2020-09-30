75dffde8253e637a500057544347c01e7c3e43eb
John Muhlenberg <jmuhlenberg16@yahoo.com>
2020-09-30 12:33:39 -0400

    updated song model

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
+const MONGODBURI = process.env.MONGODBURI
+
+
 
 // Database
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODBURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 })
+
 // Error / success
 mongoose.connection.on('error', err =>
   console.log(
@@ -18,15 +21,29 @@ mongoose.connection.on('error', err =>
     ' is Mongod not running?/Problem with Atlas Connection?'
   )
 )
+
 mongoose.connection.on('connected', () =>
-  console.log('mongo connected: ', MONGODB_URI)
+  console.log('mongo connected: ', MONGODBURI)
 )
+
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
+
+
+// Middleware
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+
+
+// Listener
 app.listen(PORT, () => {
 console.log('listening to ...', PORT);
 } )
