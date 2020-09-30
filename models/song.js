ebe82c508e2e3981e63f25e99a7cd59cc744d1e4
John Muhlenberg <jmuhlenberg16@yahoo.com>
2020-09-30 15:00:30 -0400

    edited the default video to show properly

--- models/song.js
+++ models/song.js
@@ -4,7 +4,7 @@ const songSchema = new mongoose.Schema({
   artist: {type: String, require: true},
   song: {type: String, require: true},
   rating: {type: Number, require: true, min: 0, max: 10},
-  iframe: {type: Object, default: '<iframe width="560" height="315" src="https://www.youtube.com/embed/AzV77KFsLn4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'},
+  iframe: {type: Object, default: "https://www.youtube.com/embed/AzV77KFsLn4"},
   description: String
 })
 
