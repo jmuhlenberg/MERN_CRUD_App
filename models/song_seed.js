007b150606703dab45b92678ca7604a90637d84c
John Muhlenberg <jmuhlenberg16@yahoo.com>
2020-09-30 12:44:18 -0400

    deleted the comma after description, changed iframes to backticks

--- models/song_seed.js
+++ models/song_seed.js
@@ -3,32 +3,32 @@ module.exports = [
     artist: 'Unknown',
     song: 'Baby Shark Dance',
     rating: 0,
-    iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/XqZsoesa55w?start=10" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    description: 'literally the worst song ever created',
+    iframe: `<iframe width="560" height="315" src="https://www.youtube.com/embed/XqZsoesa55w?start=10" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    description: 'literally the worst song ever created'
   },
 
   {
     artist: 'Polyphia',
     song: 'LIT',
     rating: 10,
-    iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/per9Wz0N-QA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    description: 'best instrumental track',
+    iframe: `<iframe width="560" height="315" src="https://www.youtube.com/embed/per9Wz0N-QA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    description: 'best instrumental track'
   },
 
   {
     artist: 'Smashing Pumpkins',
     song: '1979',
     rating: 7,
-    iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/4aeETEoNfOg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    description: 'classic rock song',
+    iframe: `<iframe width="560" height="315" src="https://www.youtube.com/embed/4aeETEoNfOg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    description: 'classic rock song'
   },
 
   {
     artist: 'Night Lovell',
     song: 'Alone',
     rating: 8,
-    iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/sCqz9phxFj8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    description: 'dark and deep',
+    iframe: `<iframe width="560" height="315" src="https://www.youtube.com/embed/sCqz9phxFj8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    description: 'dark and deep'
   },
 ]
 
